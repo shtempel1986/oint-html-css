ba415711aa19764d22f75ae2a0cf3ae75eda25a9
Pavel Ledovskikh <o_gredi@mail.ru>
2017-12-21 18:10:33 +0300

    Начало первой страницы готовы 5 компонентов

---
+++ src/js/scripts.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function ($) {
+
+	$(function () {
+
+		//===========================================
+		//	AUTOCOMPLETE START
+		//	=========================================
+
+		$('[data-autocomplete]').each(function () {
+
+			var data = $(this).data('autocomplete');
+
+			$(this).autocomplete({
+				source: data
+			});
+		});
+
+		//===========================================
+		//	AUTOCOMPLETE END
+		//	=========================================
+
+		//===========================================
+		//	datepicker START
+		//	=========================================
+
+		$.datepicker.regional['ru'] = {
+			closeText: 'Закрыть',
+			prevText: '&#x3c;Пред',
+			nextText: 'След&#x3e;',
+			currentText: 'Сегодня',
+			monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+			monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+			dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+			dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+			dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+			dateFormat: 'dd.mm.yy',
+			firstDay: 1,
+			isRTL: false
+		};
+		$.datepicker.setDefaults($.datepicker.regional["ru"]);
+
+		$('[data-datepicker]').datepicker({
+			maxDate: new Date(),
+			selectOtherMonths: true,
+			showOtherMonths: true
+
+		});
+
+		//===========================================
+		//	datepicker END
+		//	=========================================
+
+	});
+})(jQuery);
